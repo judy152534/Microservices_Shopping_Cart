8798a946e7df9c12631b337f6a37f8b2158c73ab
Judy Lin <judy152534@gmail.com>
2021-07-30 17:21:01 +0800

    Basket.API added

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingItem> Items { get; set; } = new List<ShoppingItem>();
+        public ShoppingCart()
+        {
+        }
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+
+        public decimal TotalPrice 
+        {
+            get 
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items) 
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
