bdb68427daa1408c0b7ea3401f6857886e567104
judy <judy152534@gmail.com>
2021-08-08 17:57:19 +0800

    add coupon entity and ICouponRepository

---
+++ src/Services/Discount/Discount.API/Repository/ICouponRepository.cs
@@ -0,0 +1,17 @@
+﻿using Discount.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetCoupon(string productName);
+        Task<bool> CreateCoupon(Coupon coupon);
+        Task<bool> UpdateCoupon(Coupon coupon);
+        Task<bool> DeleteCoupon(string productName);
+
+    }
+}
