84d7deb3e133178ca30fd48344065dce1f05eeb7
judy <judy152534@gmail.com>
2021-09-10 17:51:44 +0800

    add CheckoutOrderCommand & GetOrdersListQuery

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderHandler.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<CheckoutOrderHandler> _logger;
+
+        public CheckoutOrderHandler(IOrderRepository orderRepository, IMapper mapper, IEmailService emailService, ILogger<CheckoutOrderHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _emailService = emailService;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Order>(request);
+            var result = await _orderRepository.AddAsync(orderEntity);
+            _logger.LogInformation($"Order {result.ID} created successfully.");
+
+            await SendEmail(result);
+            return result.ID;
+        }
+
+        private async Task SendEmail(Order order) 
+        {
+            var mail = new Email() { To = "Timdd@gmail.com", Subject = "Order was create", Body = $"Hello,{order.UserName}! Your order was created." };
+            try
+            {
+                await _emailService.SendEmail(mail);
+            }
+            catch
+            {
+                _logger.LogError($"Fail to send mail to order {order.ID}: username {order.UserName}");
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            // Remember that application layer only care about business requirement (no implement)
+            var order = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(order);
+        }
+    }
+}
