2f9b244817292f7dcde97c3c522898f70f96085d
judy <judy152534@gmail.com>
2021-09-23 18:04:56 +0800

    add behaviors

---
+++ src/Services/Ordering/Ordering.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+
+namespace Ordering.Application.Behaviors
+{
+    // Accomodate all validatiors & run them
+    // Throw custom ValidationExceptions if needed
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any()) 
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validateResult = await Task.WhenAll(_validators.Select(p => p.ValidateAsync(context, cancellationToken)));
+                var failures = validateResult.SelectMany(p => p.Errors).Where( f => f != null).ToList();
+                if (failures.Count != 0) 
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+            return await next();
+        }
+    }
+}
