36b076e8f9e6f884697b87426ea3898e2ab1f70d
judy <judy152534@gmail.com>
2021-10-07 16:16:17 +0800

    Implement EmailService

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entity;
+using Ordering.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context): base(context)
+        {
+        }
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders.Where(x => x.UserName == userName).ToListAsync();
+            return orderList;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -13,7 +13,12 @@ namespace Ordering.Infrastructure.Repositories
 {
     public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
     {
-        private readonly OrderContext _dbContext;
+        // protected rather than private:for subclass to use
+        protected readonly OrderContext _dbContext;
+        public RepositoryBase(OrderContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
         public async Task<T> AddAsync(T entity)
         {
             _dbContext.Set<T>().Add(entity);
