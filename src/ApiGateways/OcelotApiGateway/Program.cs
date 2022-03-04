e449626b6a05ce86561f7d0abf6651b7c1ddf433
judy <judy152534@gmail.com>
2022-03-04 15:29:26 +0800

    1.fix CatalogDB(MangoDb) connectionStrings 2.Add Ocelot API gateway

---
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcelotApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development");
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    //config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+                    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+                    //config.AddEnvironmentVariables();   
+                })
+                .ConfigureLogging((hostingContext, loggerBuilder) => 
+                {
+                    loggerBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    loggerBuilder.AddConsole();
+                    loggerBuilder.AddDebug();
+                });
+    }
+}
